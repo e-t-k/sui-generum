b861d1566895a5aeedd7f56f7f2b4c656df2b9f2
e-t-k <e-t-k@example.com>
2016-05-12 12:30:22 -0700

    Final version for now

--- compare_tgen_mutect.py
+++ compare_tgen_mutect.py
@@ -45,6 +45,16 @@ OUTSITES_BOTH = "sites_found_in_both_files"
 OUTSITES_TGEN = "sites_found.tgen_only"
 OUTSITES_MUTECT = "sites_found.mutect_only"
 
+# derived VCFs. eventually will have headers. B = for both sites.
+OUTVCF_TGEN_B = "sites_found.both.tgen.vcf"
+OUTVCF_MUTECT_B = "sites_found.both.mutect.vcf"
+OUTVCF_TGEN = "tgen_only.sites_found.vcf"
+OUTVCF_MUTECT = "mutect_only.sites_found.vcf"
+
+# TODO : calculate outdir once and stick on to item instead of making every time
+
+
+
 def run_vcftools(item):
    outdir = out_basedir + "/" + item[0] + OUTPUT_SUFFIX
    file_info = item[1]
@@ -93,7 +103,7 @@ def make_nochr_arg(x):
 # mutect vcf lines for sites found only in that file:
 # mutect_only.sites_found.0001.vcf 
 def make_extra_files(item):
-   print item
+   #print item
    patient_id = item[0]
    outdir = out_basedir + "/" + patient_id + OUTPUT_SUFFIX + "/"
 
@@ -119,26 +129,109 @@ def make_extra_files(item):
                   # and join them up again and write.
                   which_writer.write("\t".join(cols)+os.linesep)
                   
+   # Then, generate VCF files from these sites
+   # TODO this will have to insert VCF headers copied from the original file 
+   # into the generated VCF file. For now, just make VCFs without headers.
+   generate_derived_vcfs(OUTSITES_BOTH, { 
+                                          "tgen":OUTVCF_TGEN_B, 
+                                          "mutect":OUTVCF_MUTECT_B,
+                                        },
+                                           item )
+   generate_derived_vcfs(OUTSITES_TGEN, 
+                           { "tgen":OUTVCF_TGEN,"mutect":False},
+                         item)
+   generate_derived_vcfs(OUTSITES_MUTECT,
+                           {"tgen":False,"mutect":OUTVCF_MUTECT},
+                         item)
 
 
-   # TODO do the header lines stuff later
-   # get header lines from tgen file, first.
+def generate_derived_vcfs(src_csv, which_vcfs, item):
 
-   # read out.diff.sites_in_files, parse on the IN_FILE column
-   # B -> copy to sites_found_in_both_files
-
-   # 1 -> tmp file for tgen
-   # 2 -> tmp file for mutect
-   # then. 
-   # the tmp files are CHR and POS[FOR TGEN/vs mutect - use correct column!]
-   # then for each, print header lines to make the vcf files
-   # then for each, grep -f -F with ^ to make the vcf files and append,
-   # then do a quick grep -vc to see if counts are good. 
-   #
+   #print item
 
 
+   outdir = out_basedir + "/" + item[0] + OUTPUT_SUFFIX + "/"
 
-   pass
+   # which_vcfs: { tgen: "filename" or False , mutect: "filename" or False }
+   tgen = which_vcfs["tgen"]
+   mutect = which_vcfs["mutect"]
+
+   print "Making vcfs from %s" % src_csv
+   if(tgen): print "Making %s" % tgen
+   if(mutect): print "Making %s" % mutect
+   
+   
+
+   # Set up the dialect to pass quotes, etc through verbatim
+   csv.register_dialect('vcf_file', delimiter="\t",
+                                 quotechar="",
+                                 lineterminator=os.linesep,
+                                 quoting=csv.QUOTE_NONE)
+
+
+   with open(outdir + src_csv, "rb") as src:
+      # no header so not using DictReader
+      # open additional files as appropriate -
+      # src vcf and dest vcf
+      # as csv readers as well.
+      if(tgen):
+         # input files are NOT in the outdir
+         tgen_in = csv.reader(open(item[1]['tgen_vcf'], "rb"), "vcf_file")
+         tgen_out =csv.writer(open(outdir + tgen, "wb"), "vcf_file")
+      if(mutect):
+         mutect_in = csv.reader(open(item[1]['cleaned_mutect_vcf'],"rb"),"vcf_file")
+         mutect_out = csv.writer(open(outdir + mutect, "wb"), "vcf_file")
+      reader = csv.reader(src, delimiter="\t")
+      # For each line, go through src vcfs
+      # until you either get to the end or find it
+      for fields in reader:
+         # get the cols to search on
+         # chrom and pos
+         # note for mutect, as file 2, this is a diff 2nd col.
+         tsearch = [fields[0], fields[1]]
+         msearch = [fields[0], fields[2]]
+
+         # Then check the two files until we get to it.
+         # TODO ugh writing everything twice
+         # tgen
+         if(tgen):
+            gotfields = []
+            while( gotfields != tsearch): 
+               infields = tgen_in.next()
+               # if its a header line, write it always
+               if(infields[0][0] == "#"):
+                  tgen_out.writerow(infields)
+               # if not, see if it matches the line we're looking for
+               # for both tgen & mutect this will be the first 2 cols
+               else:
+                  gotfields = [infields[0], infields[1]]
+            # ok, gotfields == tsearch OR we ran out of lines.
+            # I think if we run out of lines it will throw an error
+            # so gotfields == tsearch.
+            tgen_out.writerow(infields)
+            
+        
+          # mutect (todo after i confirm tgen works.)
+         if(mutect):
+            gotfields = []
+            while( gotfields != msearch): 
+               infields = mutect_in.next()
+               # if its a header line, write it always
+               if(infields[0][0] == "#"):
+                  mutect_out.writerow(infields)
+               # if not, see if it matches the line we're looking for
+               # for both tgen & mutect this will be the first 2 cols
+               else:
+                  gotfields = [infields[0], infields[1]]
+            # ok, gotfields == msearch OR we ran out of lines.
+            # if we run out of lines it makes a StopIteration error.
+            # so gotfields == msearch.
+            mutect_out.writerow(infields)
+ 
+
+
+   # iterate through, pull the matched columns
+   # and write to dest file(s)
 
 
 # Iterates through the mutect VCF file
@@ -147,7 +240,7 @@ def make_extra_files(item):
 # all cromosomes in CHROM column with format chr$X are changed to $X
 def clean_mutect_vcf(mutect_filename):
    cleaned_filename = mutect_filename + ".cleaned.vcf" 
-   headers = [] # TODO decouple
+   headers = [] # TODO decouple. not actually using it here.
 
 
    # Skip cleaning if a cleaned file already exists.
