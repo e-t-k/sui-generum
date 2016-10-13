3f4769a2cad55ce67a4a7419e19ad30c01ddff45
e-t-k <e-t-k@example.com>
2016-10-13 16:54:58 -0700

    also get_samplenames, leave study namespace in

--- aggregate-metadata-file/get_samplenames_from_expression.sh
+++ aggregate-metadata-file/get_samplenames_from_expression.sh
@@ -8,19 +8,17 @@ sed -e "s/\t/\n/g" < samplenames.oneline.txt > samplenames.withGene.txt
 # remove 'Gene' header
 tail -n +2 samplenames.withGene.txt > samplenames.txt
 
-# TODO - right now we are removing the study delimiters
-# ie, everything before a /
-#  ultimately, want to leave them in, I think
-# but right now they're not in the metadata tsvs.
-sed -e "s/.*\///" < samplenames.txt > samples.from.expression.txt
+# Don't remove the study delimiters.
+mv samplenames.txt samples.from.expression.txt
 
-# TODO - if we're removing the study delimiters,
+# If we DO remove study delimiters, see below.
+#sed -e "s/.*\///" < samplenames.txt > samples.from.expression.txt
+
+# if we're removing the study delimiters,
 # we also have to remove them from the expression TSV as well
 # do this manually. eg for delimiters ABCD and ABCD_reference:
-
 # head -n 1 expression.tsv > header.expression
 # sed -e "s/ABCD\///g" -e "s/ABCD_reference\///g" < header.expression > header.fixed
-
 # Then replace the first line with header.fixed
 
 
